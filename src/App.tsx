2f52570eccaa178992c829442fe5446660b51327
ShayMashiah <shay2908@gmail.com>
2025-05-19 20:06:36 +0300

    feat: connect handleClick to an API get request based on the city name

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import { useState } from "react";
 import { Box, Typography, TextField, Button } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const [city, setCity] = useState("");
 
-  const handleClick = async () => {};
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+  const handleClick = async (cityName: string = city) => {
+        if (!city) return;
+    try {
+      const response = await axios.get(
+        `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}`
+      );
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+    console.log("City name:", cityName);
+  };
+
 
   return (
     <>
@@ -60,7 +74,7 @@ function App() {
         />
 
         <Button
-          onClick={handleClick}
+          onClick={() => handleClick()}
           sx={{
             bgcolor: "#374151",
             color: "#fff",
