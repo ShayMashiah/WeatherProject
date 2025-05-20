9e01dff1967f36709ed9f12dbbab09bdfd07f04a
ShayMashiah <shay2908@gmail.com>
2025-05-20 11:27:38 +0300

    fix: fetch data using React Query

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,30 @@ import { useState } from "react";
 import { Box, Typography, TextField, Button } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import "./App.css";
-import axios from "axios";
+import { fetchWeather } from "./weatherApi";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
   const [city, setCity] = useState("");
 
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-  const handleClick = async (cityName: string = city) => {
-        if (!city) return;
-    try {
-      const response = await axios.get(
-        `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}`
-      );
-    } catch (error) {
-      console.error("Error fetching weather data:", error);
+  const {
+    data: 
+    weatherData,
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery({
+    queryKey: ["weather", city],
+    queryFn: () => fetchWeather(city, API_KEY),
+    enabled: false,
+  });
+
+  const handleClick = () => {
+    if (city) {
+      refetch();
     }
-    console.log("City name:", cityName);
   };
 
   return (
